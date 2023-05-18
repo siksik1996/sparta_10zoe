0313c88271701de1c88d12a89c50c087cb86b3c6
nbcnvc <nbcnvc.dev@gmail.com>
2023-05-18 14:34:37 +0900

    음원 순위 api 요청시 크롤링 하도록 변경

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
+import requests
+
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
 from flask import Flask, jsonify
 from flask_cors import CORS
-
-from pymongo import MongoClient
 
 
 app = Flask(__name__)
@@ -11,9 +14,29 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 client = MongoClient('mongodb://root:root@db/?retryWrites=true&w=majority')
 db = client.dbsparta
 
+def crawl_musics():
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get('https://www.genie.co.kr/chart/top200', headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+    for tr in trs:
+        title = tr.select_one('td.info > a.title.ellipsis').text.strip()
+        rank = tr.select_one('td.number').text[0:2].strip()
+        artist = tr.select_one('td.info > a.artist.ellipsis').text
+        doc = {
+            'title':title,
+            'rank':rank,
+            'artist':artist
+        }
+        db.music.insert_one(doc)
+
 
 @app.route("/musics/", methods=["GET"])
 def get_music_list():
+    crawl_musics()
     all_music = list(db.music.find({},{'_id':False}))[:5]
     return jsonify(all_music)
 
