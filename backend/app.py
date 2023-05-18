799bb2221902725028c450e6c3129fcf0cacd1d6
nbcnvc <nbcnvc.dev@gmail.com>
2023-05-18 14:10:53 +0900

    벡엔드 환경을 도커로 변경

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 from pymongo import MongoClient
 
 
 app = Flask(__name__)
 
-client = MongoClient()
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+client = MongoClient('mongodb://root:root@db/?retryWrites=true&w=majority')
 db = client.dbsparta
 
 
 @app.route("/musics/", methods=["GET"])
 def get_music_list():
-    all_music = list(db.music.find({},{'_id':False}))
-    return jsonify({'result': all_music})
+    all_music = list(db.music.find({},{'_id':False}))[:5]
+    return jsonify(all_music)
 
 
 if __name__ == '__main__':
