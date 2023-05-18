d8c5daceb3cc87474d0fe6ebe88e8a7eb24d63b8
kkotburi <kkotburiwillbloom@gmail.com>
2023-05-18 14:34:55 +0900

    third commit

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,26 @@ def get_music_list():
     all_music = list(db.music.find({},{'_id':False}))
     return jsonify({'result': all_music})
 
+@app.route("/comments", methods=["POST"])
+def comments_post():
+    name_receive = request.form['name_give']
+    emoji_receive = request.form['emoji_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'name': name_receive,
+        'emoji': emoji_receive,
+        'comment': comment_receive
+    }
+
+    db.comments.insert_one(doc)
+
+    return jsonify({'msg':'고맙습니다:)'})
+
+@app.route("/comments", methods=["GET"])
+def comments_get():
+    comments_data = list(db.comments.find({},{'_id':False}))
+    return jsonify({'result':comments_data})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8080, debug=True)
