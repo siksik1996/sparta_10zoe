29655c4752a76960df5b7d771abb55675bf8cdcf
nbcnvc <nbcnvc.dev@gmail.com>
2023-05-18 15:42:07 +0900

    방명록에 대한 api 구현

--- backend/app.py
+++ backend/app.py
@@ -51,34 +51,14 @@ def guestbook_post():
     }
     db.fan.insert_one(doc)
 
-    return jsonify({'msg': 'save completed'})
+    return jsonify({'msg': '감사합니다!'})
 
 
 @app.route("/guestbook/comments", methods=["GET"])
 def guestbook_get():
-    comments = list(db.fan.find({},{'_id':False}))
+    comments = list(db.fan.find({}, {'_id':False}))
     return jsonify(comments)
 
-@app.route("/comments", methods=["POST"])
-def comments_post():
-    name_receive = request.form['name_give']
-    emoji_receive = request.form['emoji_give']
-    comment_receive = request.form['comment_give']
-
-    doc = {
-        'name': name_receive,
-        'emoji': emoji_receive,
-        'comment': comment_receive
-    }
-
-    db.comments.insert_one(doc)
-
-    return jsonify({'msg':'고맙습니다:)'})
-
-@app.route("/comments", methods=["GET"])
-def comments_get():
-    comments_data = list(db.comments.find({},{'_id':False}))
-    return jsonify({'result':comments_data})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8080, debug=True)
