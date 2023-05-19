42e30eb52fc37384695f2dba036cb899c89a70a5
nbcnvc <nbcnvc.dev@gmail.com>
2023-05-19 09:50:04 +0900

    Change backend url

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let baseUrl = 'http://localhost:8080';
+let baseUrl = 'http://0d86-59-13-68-52.ngrok-free.app';
 
 let titles = document.querySelectorAll('.song-title');
 let artists = document.querySelectorAll('.artist-name');
 
 // let realTime = document.querySelector('.real-time');
 // realTime.innerHTML = new Date().toLocaleTimeString();
-	// 현재 시각 표시
+// 현재 시각 표시
 function updateClock() {
 	const now = new Date();
 	const year = now.getFullYear();
@@ -15,15 +15,15 @@ function updateClock() {
 	const hours = String(now.getHours()).padStart(2, '0');
 	const minutes = String(now.getMinutes()).padStart(2, '0');
 	const seconds = String(now.getSeconds()).padStart(2, '0');
-	
+
 	const dateStr = `${year}.${month}.${day}(${dayOfWeek})`;
 	const timeStr = `${hours}:${minutes}:${seconds}`;
-	
+
 	document.getElementById('date').textContent = dateStr;
 	document.getElementById('time').textContent = timeStr;
 }
-	  
-	  // 매 초마다 시간 업데이트
+
+// 매 초마다 시간 업데이트
 setInterval(updateClock, 1000);
 
 $(document).ready(function () {
